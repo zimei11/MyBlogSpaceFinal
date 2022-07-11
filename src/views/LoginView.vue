697b9e5c0c0413d888acd263fa5dd042b8e786be
zimei li <2928577064@qq.com>
2022-07-11 21:09:23 +0800

    登录跳转

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,73 +1,91 @@
 <template>
-    <div class="common-layout">
+  <div class="common-layout">
 
-        <el-container>
+    <el-container>
 
-            <el-main>
-                <h1 style="text-align:center;">用户登录</h1>
-                <el-divider />
-                <el-row>
-                    <el-col :xs="6" :sm="8" :md="9">
-                        <div class="grid-content ep-bg-purple"></div>
-                    </el-col>
-                    <el-col :xs="12" :sm="8" :md="6">
+      <el-main>
+        <h1 style="text-align:center;">用户登录</h1>
+        <el-divider/>
+        <el-row>
+          <el-col :xs="6" :sm="8" :md="9">
+            <div class="grid-content ep-bg-purple"></div>
+          </el-col>
+          <el-col :xs="12" :sm="8" :md="6">
 
-                        <el-card>
-                            <el-form :model="user" label-width="120px" label-position="top">
-                                <el-form-item label="用户名">
-                                    <el-input type="text" v-model="user.username" />
-                                </el-form-item>
-                                <el-form-item label="密码">
-                                    <el-input type="password" v-model="user.password" />
-                                </el-form-item>
-                                <el-form-item>
-                                    <el-col :xs="24" :sm="12">
-                                        <el-button class="botton" type="primary" @click="login" style="width:100%">登录
-                                        </el-button>
-                                    </el-col>
-                                    <el-col :xs="24" :sm="12">
-                                        <el-button class="botton" style="width:100%">取消</el-button>
-                                    </el-col>
-                                </el-form-item>
-                            </el-form>
-                        </el-card>
-                    </el-col>
-                    <el-col :xs="6" :sm="8" :md="9">
-                        <div class="grid-content ep-bg-purple"></div>
-                    </el-col>
-                </el-row>
-            </el-main>
-        </el-container>
-    </div>
+            <el-card>
+              <el-form label-width="120px" label-position="top">
+                <el-form-item label="用户名">
+                  <el-input type="text" v-model="username" placeholder="输入用户名"/>
+                </el-form-item>
+                <el-form-item label="密码">
+                  <el-input type="password" v-model="password" placeholder="输入密码"/>
+                </el-form-item>
+                <p style="color:red" class="error-message">{{ error_message }}</p>
+                <el-form-item>
+                  <el-col :xs="24" :sm="12">
+                    <el-button class="button" type="primary" @click="login" style="width:100%">登录
+                    </el-button>
+                  </el-col>
+                  <el-col :xs="24" :sm="12">
+                    <el-button class="button" style="width:100%">取消</el-button>
+                  </el-col>
+                </el-form-item>
+              </el-form>
+            </el-card>
+          </el-col>
+          <el-col :xs="6" :sm="8" :md="9">
+            <div class="grid-content ep-bg-purple"></div>
+          </el-col>
+        </el-row>
+      </el-main>
+    </el-container>
+  </div>
 </template>
 
 <script setup>
-import { reactive } from 'vue'
+import {ref} from 'vue'
+import {useStore} from 'vuex'
+import router from "@/router/index";
 
-// do not use same name with ref
-const user = reactive({
-    username: '',
-    password: '',
-})
+const store = useStore();
+let username = ref("");
+let password = ref("");
+let error_message = ref("");
+
+const login = () => {
+  error_message.value="";
+  store.dispatch("login", {
+    username: username.value,
+    password: password.value,
+    success() {
+      router.push({name: 'home'});
+    },
+    error() {
+      error_message.value = "用户名或密码错误";
+    }
+  })
+  // console.log(username.value,password.value);
+};
 </script>
 
 <style scoped>
-.botton {
-    margin-bottom: 10px;
+.button {
+  margin-bottom: 10px;
 }
 
 .el-container {
-    height: 100%;
+  height: 100%;
 }
 
 .el-main {
-    height: 100%;
-    background: linear-gradient(200deg, #f3e7e9, #e3eeff);
+  height: 100%;
+  background: linear-gradient(200deg, var(--el-color-danger-light-8), var(--el-color-primary-light-5));
 }
 
 .common-layout {
-    height: 100%;
+  height: 100%;
 }
+
 /* .el-card{
     background-image: url('https://img.zimei.xyz/202207101321009.jpg');
     background-size:100%;
