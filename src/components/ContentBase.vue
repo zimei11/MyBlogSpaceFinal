964387c58dfa2b4423b13e0e3f4a630b299fd78b
zimei li <2928577064@qq.com>
2022-07-11 09:43:37 +0800

    导航栏固定，添加黑暗模式

--- src/components/ContentBase.vue
+++ src/components/ContentBase.vue
@@ -1,14 +1,15 @@
 <template>
-    <el-backtop :right="20" :bottom="50" />
     <div class="common-layout">
         <el-container>
-            <el-header>
-                <TopMenuVue />
-            </el-header>
+            <el-affix :offset="0">
+                <el-header>
+                    <TopMenuVue />
+                </el-header>
+            </el-affix>
             <el-main>
                 <el-row :gutter="0">
                     <el-col :span="24">
-                        <el-card shadow="never" style="background-color:#f2f2f5">
+                        <el-card shadow="never" style="background-color:var(--el-color-info-light-9)">
                             <slot></slot>
                         </el-card>
                     </el-col>
@@ -19,12 +20,23 @@
             </el-footer>
         </el-container>
     </div>
+    <!-- 暗黑模式 -->
+    <el-affix position="bottom" :offset="50">
+        <el-button @click="toggleDark()" circle size="large">
+            <el-icon color="var(--el-color-primary-dark-2)">
+                <Moon />
+            </el-icon>
+        </el-button>
+    </el-affix>
+    <!-- 回到顶部 -->
+    <el-backtop :right="20" :bottom="100"  />
 </template>
 
 <script setup>
 import FooterMenuVue from './Menu/FooterMenu.vue';
 import TopMenuVue from './Menu/TopMenu.vue';
-
+import { toggleDark } from '@/composables/dark.js'
+import { Moon } from '@element-plus/icons-vue'
 
 </script>
 
